e478c7de10eb183ac20e68176b9f2c49b1e07470
DEIVISS <99610648+Lizzen@users.noreply.github.com>
2023-06-13 18:12:39 +0200

    Laboratorios

---
+++ Labs/labs/lab14/Debug/lab14-isr-wrappers.d
@@ -0,0 +1,1 @@
+lab14-isr-wrappers.d lab14-isr-wrappers.o: ../lab14-isr-wrappers.asm
---
+++ Labs/labs/lab14/Debug/lab14.d
@@ -0,0 +1,40 @@
+lab14.d lab14.o: ../lab14.c ../os_port/os_cpu.h \
+ ../../BSP/include/common_types.h ../../BSP/include/system.h ../os_cfg.h \
+ ../os_core/ucos_ii.h ../os_cfg.h ../../BSP/include/s3c44b0x.h \
+ ../../BSP/include/s3cev40.h ../../BSP/include/leds.h \
+ ../../BSP/include/segs.h ../../BSP/include/uart.h \
+ ../../BSP/include/pbs.h ../../BSP/include/keypad.h \
+ ../../BSP/include/timers.h ../../BSP/include/rtc.h \
+ ../../BSP/include/lcd.h
+
+../os_port/os_cpu.h:
+
+../../BSP/include/common_types.h:
+
+../../BSP/include/system.h:
+
+../os_cfg.h:
+
+../os_core/ucos_ii.h:
+
+../os_cfg.h:
+
+../../BSP/include/s3c44b0x.h:
+
+../../BSP/include/s3cev40.h:
+
+../../BSP/include/leds.h:
+
+../../BSP/include/segs.h:
+
+../../BSP/include/uart.h:
+
+../../BSP/include/pbs.h:
+
+../../BSP/include/keypad.h:
+
+../../BSP/include/timers.h:
+
+../../BSP/include/rtc.h:
+
+../../BSP/include/lcd.h:
